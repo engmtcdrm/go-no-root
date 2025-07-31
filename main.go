e2bb302ab82f2c8c95596e89798ba78f38be97b9
engmtcdrm <deedsoftheforsaken@gmail.com>
2025-07-30 22:39:15 -0400

    More craziness

--- main.go
+++ main.go
@@ -1,14 +1,41 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"os/exec"
 	"os/user"
+	"slices"
 	"strconv"
-	"strings"
 )
+
+func traverseProcessTree(startPid int, currentUser *user.User) error {
+	bannedCommands := []string{"sudo", "su", "doas", "super", "calife", "pkexec", "systemd-run", "machinectl", "runuser"}
+
+	for pid := startPid; pid > 1; {
+		proc, err := GetProcessByPid(pid)
+		if err != nil {
+			break
+		}
+
+		cUid, err := strconv.Atoi(currentUser.Uid)
+		// If same user, continue up the tree
+		if proc.Uid() == cUid {
+			pid = proc.Ppid()
+			continue
+		}
+
+		// If root process, check for banned commands
+		if proc.Uid() == 0 {
+			if slices.Contains(bannedCommands, proc.Binary()) {
+				return fmt.Errorf("detected privilege escalation via %s", proc.Binary())
+			}
+		}
+
+		pid = proc.Ppid()
+	}
+	return nil
+}
 
 func isSetUid(filename string) (bool, error) {
 	info, err := os.Stat(filename)
@@ -20,16 +47,34 @@ func isSetUid(filename string) (bool, error) {
 }
 
 func main() {
+	cUser, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Current User: %s (UID: %s)\n\n", cUser.Username, cUser.Uid)
+
+	goproc, err := GetProcessByPid(os.Getpid())
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf(
+		"Go Process Info:\n  PID: %d\n  PPID: %d\n  UID: %d\n  GID: %d\n  Binary: %s\n  Cmdline: %v\n\n",
+		goproc.Pid(),
+		goproc.Ppid(),
+		goproc.Uid(),
+		goproc.Gid(),
+		goproc.Binary(),
+		goproc.Cmdline(),
+	)
+
 	rprocs, err := GetUserProcesses("root")
 	if err != nil {
 		panic(err)
 	}
 
-	cUser, err := user.Current()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Current User: %s (UID: %s)\n", cUser.Username, cUser.Uid)
+	fmt.Print("Root Processes:\n\n")
 
 	for _, proc := range rprocs {
 		// fmt.Println("PID:", proc.pid, " \tUID:", proc.uid, " \tGID:", proc.gid, " \tBinary:", proc.binary, "\tCmdline:", proc.cmdline)
@@ -45,13 +90,20 @@ func main() {
 		}
 
 		if isSetuid {
-			fmt.Printf("UID: %d\tIs %s setuid? %v\tCmdline: %s\n", proc.uid, cmdPath, isSetuid, proc.cmdline)
+			fmt.Printf("  Command: %s\n", cmdPath)
+			fmt.Printf(
+				"    PID: %d\n    PPID: %d\n    UID: %d\n    Cmdline: %s\n    Is %s setuid? %v\n\n",
+				proc.pid,
+				proc.ppid,
+				proc.uid,
+				cmdPath,
+				isSetuid,
+				proc.cmdline,
+			)
 
 			cUid, _ := strconv.Atoi(cUser.Uid)
-
-			if strings.Contains(proc.cmdline, cUser.Username) && proc.uid != cUid {
-				fmt.Printf("Found process with current user in cmdline: %s\n", proc.cmdline)
-				panic(errors.New("found process with current user in cmdline"))
+			if slices.Contains(proc.cmdline, cUser.Username) && proc.uid != cUid {
+				panic(fmt.Errorf("found process with current user in it %v", proc))
 			}
 		}
 
--- ps.go
+++ ps.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"os/user"
+	"path"
 	"regexp"
 	"strconv"
 	"strings"
@@ -13,10 +14,36 @@ import (
 
 type Process struct {
 	pid     int
+	ppid    int
+	state   string
 	uid     int
 	gid     int
 	binary  string
-	cmdline string
+	cmdline []string
+}
+
+func (p *Process) Pid() int {
+	return p.pid
+}
+
+func (p *Process) Ppid() int {
+	return p.ppid
+}
+
+func (p *Process) Uid() int {
+	return p.uid
+}
+
+func (p *Process) Gid() int {
+	return p.gid
+}
+
+func (p *Process) Binary() string {
+	return p.binary
+}
+
+func (p *Process) Cmdline() []string {
+	return p.cmdline
 }
 
 func getUIDFromUser(username string) (int, error) {
@@ -31,6 +58,65 @@ func getUIDFromUser(username string) (int, error) {
 	}
 
 	return uid, nil
+}
+
+func GetProcessByPid(pid int) (*Process, error) {
+	pidDir := fmt.Sprintf("/proc/%d", pid)
+
+	procStat, err := os.Stat(pidDir)
+	if err != nil {
+		return nil, fmt.Errorf("pid %d: does not exist", pid)
+	}
+
+	if !procStat.IsDir() {
+		return nil, fmt.Errorf("could not get pid information, pid %d", pid)
+	}
+
+	statPath := path.Join(pidDir, "stat")
+	statBytes, err := os.ReadFile(statPath)
+	if err != nil {
+		return nil, fmt.Errorf("could not read stat file for PID %d: %v", pid, err)
+	}
+
+	stats := strings.TrimSpace(string(statBytes))
+	binStart := strings.IndexRune(stats, '(') + 1
+	binEnd := strings.IndexRune(stats[binStart:], ')')
+	binary := stats[binStart : binStart+binEnd]
+	statSlice := strings.Split(strings.TrimSpace(stats[binStart+binEnd+1:]), " ")
+
+	if len(statSlice) < 2 {
+		return nil, fmt.Errorf("invalid stat format for PID %d", pid)
+	}
+
+	state := statSlice[0]
+	ppid, _ := strconv.Atoi(statSlice[1])
+
+	cmdlinePath := path.Join(pidDir, "cmdline")
+	cmdlineBytes, err := os.ReadFile(cmdlinePath)
+	if err != nil {
+		return nil, fmt.Errorf("could not read cmdline file for PID %d: %v", pid, err)
+	}
+
+	cmdline := strings.Split(string(cmdlineBytes), "\x00")
+
+	dsys, err := os.Stat(pidDir)
+	if err != nil {
+		return nil, fmt.Errorf("could not stat process directory for PID %d: %v", pid, err)
+	}
+	stat, ok := dsys.Sys().(*syscall.Stat_t)
+	if !ok {
+		return nil, fmt.Errorf("could not get syscall stat for PID %d", pid)
+	}
+
+	return &Process{
+		pid:     pid,
+		uid:     int(stat.Uid),
+		gid:     int(stat.Gid),
+		binary:  binary,
+		cmdline: cmdline,
+		state:   state,
+		ppid:    ppid,
+	}, nil
 }
 
 func GetUserProcesses(username string) ([]Process, error) {
@@ -60,7 +146,7 @@ func GetProcesses() ([]Process, error) {
 	}
 	defer pdir.Close()
 
-	// Regular expression to match numeric directory names
+	// Regex to match numeric directory names
 	reDigit := regexp.MustCompile(`^\d+$`)
 
 	procs := []Process{}
@@ -77,46 +163,18 @@ func GetProcesses() ([]Process, error) {
 			if !reDigit.MatchString(dir.Name()) {
 				continue
 			}
-			// syscall.Stat_t
-			dsys := dir.Sys()
 
-			stat, ok := dsys.(*syscall.Stat_t)
-
-			// Type assert to *syscall.Stat_t
-			if !ok {
-				continue
-			}
-
-			// Only care about root processes
-			// if stat.Uid != 0 {
-			// 	continue
-			// }
-
-			pid, err := strconv.ParseInt(dir.Name(), 10, 0)
+			pid, err := strconv.Atoi(dir.Name())
 			if err != nil {
 				continue
 			}
 
-			statPath := fmt.Sprintf("/proc/%d/stat", pid)
-			dataBytes, err := os.ReadFile(statPath)
+			proc, err := GetProcessByPid(pid)
 			if err != nil {
 				continue
 			}
 
-			data := string(dataBytes)
-			binStart := strings.IndexRune(data, '(') + 1
-			binEnd := strings.IndexRune(data[binStart:], ')')
-			binary := data[binStart : binStart+binEnd]
-
-			cmdlinePath := fmt.Sprintf("/proc/%d/cmdline", pid)
-			cmdlineBytes, err := os.ReadFile(cmdlinePath)
-			if err != nil {
-				continue
-			}
-
-			cmdline := strings.Replace(string(cmdlineBytes), "\x00", " ", -1)
-
-			procs = append(procs, Process{pid: int(pid), uid: int(stat.Uid), gid: int(stat.Gid), binary: binary, cmdline: cmdline})
+			procs = append(procs, *proc)
 		}
 
 	}
